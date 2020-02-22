172d0e3bd0faead92cb57dd2ba37517584096863
Hassan Jamila <hassan.jamila@n2nconnect.com>
2020-02-22 15:18:53 +0800

    Added functionality when long press on the map add new pin

--- Virtual Tourist/Controller/MapViewController.swift
+++ Virtual Tourist/Controller/MapViewController.swift
@@ -7,22 +7,59 @@
 //
 
 import UIKit
-
+import MapKit
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        FlickerApiCaller.searchForGeo(latitude: 3.194045, longitude: 101.675362) { (photos, error) in
-            guard error == nil else {
-                print (error)
-                return
-            }
-            print (photos)
-        }
+        var GestRecognize = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotation))
+        GestRecognize.minimumPressDuration = 1.0
+        mapView.addGestureRecognizer(GestRecognize)
     }
     
 
     
-
+    @objc func addAnnotation(gestRecognizer:UIGestureRecognizer){
+        print ("Long press ")
+        let id = "Pin"
+        //var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: id) as! MKPinAnnotationView
+       // if pinView == nil {
+          let   pin = MKPointAnnotation()
+        let touchLoc = gestRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(touchLoc, toCoordinateFrom: mapView)
+        pin.coordinate = coordinate
+        print (touchLoc)
+            
+        mapView.addAnnotation(pin)
+        
+        
+    }
+    
 }
+
+
+
+extension MapViewController:MKMapViewDelegate{
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let id = "Pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: id ) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: id)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }else {
+            pinView!.annotation = annotation
+        }
+        return pinView
+    }
+    
+    
+}
+
+
+
+
